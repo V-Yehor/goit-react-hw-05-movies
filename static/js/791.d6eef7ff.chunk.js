a592a631370d2a69656dbeb7d50f9ec8a2e7f7d4
V-Yehor <130846410+V-Yehor@users.noreply.github.com>
2023-12-01 21:25:39 +0000

    Deploying to gh-pages from @ V-Yehor/goit-react-hw-05-movies@850ab7f1efa69addd4e424417ead44ebd423d426 🚀

--- static/js/791.d6eef7ff.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{680:function(e,r,t){t.r(r),t.d(r,{default:function(){return a}});var n=t(87),o=t(184);function a(){return(0,o.jsxs)("div",{children:["Sorry! Please go ",(0,o.jsx)(n.rU,{to:"/",children:"Home"})," page"]})}}}]);
-//# sourceMappingURL=791.d6eef7ff.chunk.js.map
